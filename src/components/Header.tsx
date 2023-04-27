64bffef578c6f255911c83b599cce1367d2dfe60
Angel Garcia <angelgarciaweb@gmail.com>
2023-04-27 14:40:16 -0400

    ✨ feat: add main header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import type { MainHeader } from "~/interfaces/headers";
+
+
+const Header: React.FC<MainHeader> = ({ title, image, cover }) => {
+    return (
+        <header className="mt-6">
+            <Image src={ cover } width={1200} height={264} alt="cover JVN García" className="rounded-xl shadow-xl" />
+            <div className="-mt-12 flex justify-center">
+                <Image src={ image } width={124} height={124} alt="cover JVN García" className="rounded-full shadow-xl" />
+            </div>
+            <h1 className="text-center mt-8 text-4xl font-semibold">{ title }</h1>
+        </header>
+    )
+}
+
+export default Header;
