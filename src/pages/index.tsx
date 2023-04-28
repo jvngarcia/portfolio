6175e5386f974348ee0aadf385c78d278adb4c08
Angel Garcia <angelgarciaweb@gmail.com>
2023-04-28 10:48:18 -0400

    ✨ feat: add landing page base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,27 +35,52 @@ const Home: NextPage = () => {
           <div className="mt-4 grid md:grid-cols-2 grid-cols-1 gap-10">
             <article className=" hover:bg-slate-200 transition-all ease-linear rounded-md h-full">
               <Link href="/">
-                <Image src="/images/cover.jpg" width={1200} height={264} alt="cover JVN García" className="rounded-xl" />
-                <div className="mt-3 mx-3">
-                  <h4 className="truncate text-xl font-semibold">Title post</h4>
-                  <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                  <p className="text-sm mt-2">Feb, 2023</p>
-                  <div className="grid grid-flow-col auto-cols-max mt-3 gap-1">
-                    <CategoriesDecorator from="from-slate-100" to="to-slate-300" slug="#"> Nextjs </CategoriesDecorator>
-                    <CategoriesDecorator from="from-purple-300" to="to-pink-300" slug="#"> Javascript </CategoriesDecorator>
-                  </div>
-                </div>
+                <Image src="/images/cover.jpg" width={1200} height={264} alt="cover JVN García" className="rounded-xl" priority />
               </Link>
+              <div className="mt-3 mx-3 pb-6">
+                <Link href="/">
+                  <h4 className="truncate text-xl font-semibold">Title post</h4>
+                </Link>
+                <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                <p className="text-sm mt-2">Feb, 2023</p>
+                <div className="grid grid-flow-col auto-cols-max mt-3 gap-1">
+                  <CategoriesDecorator from="from-slate-100" to="to-slate-300" slug="#"> Nextjs </CategoriesDecorator>
+                  <CategoriesDecorator from="from-purple-300" to="to-pink-300" slug="#"> Javascript </CategoriesDecorator>
+                </div>
+              </div>
             </article>
           </div>
         </section>
 
         <section className="container mt-12">
-          <div className="w-full bg-slate-200 inline-flex px-6 py-6 gap-2 items-center">
-            <span className="text-xl">💡</span> 
-            <div>This site is open source and easy to fork.</div>
+          <div className="w-full bg-slate-100 inline-flex px-6 py-6 gap-2 items-center border">
+            <span className="text-xl">💡</span>
+            <div>Este sitio es de <Link href="https://github.com/jvngarcia/portfolio" target="_blank" className="hover:text-slate-400 transition-all">código abierto y fácil de usar</Link>.</div>
           </div>
         </section>
+
+        <section className="container mt-8">
+          <div className="grid md:grid-cols-3 grid-cols-2 justify-center m-auto">
+            <Link href="/" className="inline-flex items-center hover:bg-slate-100 transition-all text-center justify-center py-4">
+              <span className="text-xl mr-2">👋</span>
+              <span className="border-b font-semibold">Sobre mi</span>
+            </Link>
+
+            <Link href="/" className="inline-flex items-center hover:bg-slate-100 transition-all text-center justify-center py-4">
+              <span className="text-xl mr-2">📘</span>
+              <span className="border-b font-semibold">Blog</span>
+            </Link>
+
+            <Link href="/" className="inline-flex items-center hover:bg-slate-100 transition-all text-center justify-center py-4">
+              <span className="text-xl mr-2">✉️</span>
+              <span className="border-b font-semibold">Contacto</span>
+            </Link>
+          </div>
+        </section>
+
+        <footer className="container my-8 py-6 text-center">
+            <p>Desarrollado con <span className="text-red-500">❤️</span> por JVN García</p>
+        </footer>
       </main>
     </>
   );
