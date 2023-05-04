c10084fc60747f7f1e2eb8f2e418a3118f76c390
Angel Garcia <angelgarciaweb@gmail.com>
2023-05-04 15:55:26 -0400

    🐛 fix: error in data types

--- src/interfaces/Post.tsx
+++ src/interfaces/Post.tsx
@@ -2,7 +2,13 @@
 
 export type Post = {
     title: string,
-    extact: string,
+    content: string | null,
+    image: string,
+}
+
+export type PostArticle = {
+    title: string,
+    extract: string | null,
     slug: string,
     image: string,
 }
